eed4fd26ee73d5c09b9c7c8b79b17ef5f0d585bf
K-Muni <rocktharunraj1@gmail.com>
2024-05-13 20:09:28 +0530

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import login from './login';
+
+
+function App() {
+  return (
+    <div>
+      <login />
+      {/* <p>HII</p> */}
+    </div>
+  )
+}
+
+export default App;
